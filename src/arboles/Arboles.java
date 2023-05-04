1eb6b5a4117417614c8d2978982202c6e6c9db74
HP PORTATIL <HP PORTATIL@HPPORTATIL>
2023-05-04 11:25:20 -0500

    Navegacion de tareas añadida

--- src/arboles/Arboles.java
+++ src/arboles/Arboles.java
@@ -4,6 +4,8 @@
  */
 package arboles;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HP PORTATIL
@@ -11,11 +13,11 @@ package arboles;
 public class Arboles {
 
     public static class Node {
-        int data;
+        String data;
         Node left;
         Node right;
 
-        public Node(int data) {
+        public Node(String data) {
             this.data = data;
             this.left = null;
             this.right = null;
@@ -29,22 +31,111 @@ public class Arboles {
             root = null;
         }
 
-        public void insert(int data) {
-            root = insertRecursive(root, data);
+        public void IniciarInterfaz(){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Nombra la tarea Base");
+            System.out.print("-- ");
+            
+            root = new Node(in.nextLine());
+            root = InterfazNodo(root,"");
         }
-
-        private Node insertRecursive(Node current, int data) {
-            if (current == null) {
-                return new Node(data);
+        
+        private Node InterfazNodo(Node node,String Ruta){
+            Scanner in = new Scanner(System.in);
+            String ThisRuta;
+            
+            var i = true;
+            System.out.println("--------------------------------------------------------------------------");
+            while(i){
+                ThisRuta = Ruta+"/"+node.data;
+                System.out.println("Ruta: "+ThisRuta);
+                System.out.println("\nTarea: "+node.data);
+                
+                if(!(node.right == null && node.left == null)){
+                    System.out.println("Subtareas:");
+                    if(!(node.right == null)){
+                        System.out.println("\t- "+node.right.data);
+                    }
+                    if(!(node.left == null)){
+                        System.out.println("\t- "+node.left.data);
+                    }
+                }
+                
+                System.out.println("\nSelecciona alguna de las opcciones ingresando un numero");
+                System.out.println("1. Modificar Esta tarea");
+                System.out.println("2. Ingresar a una subtarea");
+                System.out.println("3. Volver a la macro tarea");
+                if(node.right == null || node.left == null){
+                    System.out.println("4. Añadir nueva subtarea");
+                }
+                System.out.print("-- ");
+                int Selection = in.nextInt();
+                System.out.println("\n");
+                in.nextLine();
+                switch(Selection){
+                    case (1):
+                        System.out.println("Ingresa el nuevo nombre de la tarea");
+                        System.out.print("-- ");
+                        node.data = in.nextLine();
+                        break;
+                    case (2):
+                        if(node.right == null && node.left == null){
+                            System.out.println("*No hay subtareas*");
+                        }else{
+                            int numerador = 1;
+                            if(!(node.right == null)){
+                                System.out.println(numerador+". "+node.right.data);
+                                numerador++;
+                            }
+                            if(!(node.left == null)){
+                                System.out.println(numerador+". "+node.left.data);
+                            }
+                            
+                            System.out.println("-- ");
+                            int Select = in.nextInt();
+                            switch(Select){
+                                case(1):
+                                    node.right = InterfazNodo(node.right,ThisRuta);
+                                    break;
+                                case(2):
+                                    if(numerador == 2){
+                                        node.left = InterfazNodo(node.left,ThisRuta);
+                                    }else{
+                                        System.out.println("Valor No valido");
+                                    }
+                                    break;
+                                default:
+                                    System.out.println("Valor No valido");
+                                    break;
+                            }
+                        }
+                        break;
+                    case (3):
+                        i = false;
+                        break;
+                    case(4):
+                        if(node.right == null || node.left == null){
+                            System.out.println("Ingresa el nombre de la nueva tarea");
+                            System.out.print("-- ");
+                            String nombre = in.nextLine();
+                            
+                            if(!(node.right == null)){
+                                node.left = new Node(nombre);
+                            }else
+                                node.right = new Node(nombre);
+                        }else{
+                            System.out.println("Valor No valido");
+                        }
+                        break;
+                    default:
+                        System.out.println("Valor No valido");
+                        break;
+                }
             }
-            if (data < current.data) {
-                current.left = insertRecursive(current.left, data);
-            } else if (data > current.data) {
-                current.right = insertRecursive(current.right, data);
-            }
-            return current;
+            System.out.println("--------------------------------------------------------------------------");
+            return node;
         }
-
+        
         public void inorderTraversal() {
             inorderRecursive(root);
         }
@@ -84,15 +175,7 @@ public class Arboles {
 
     public static void main(String[] args) {
             BinaryTree binaryTree = new BinaryTree();
-            binaryTree.insert(8);
-            binaryTree.insert(3);
-            binaryTree.insert(1);
-            binaryTree.insert(6);
-            binaryTree.insert(4);
-            binaryTree.insert(7);
-            binaryTree.insert(10);
-            binaryTree.insert(14);
-            binaryTree.insert(13);
+            binaryTree.IniciarInterfaz();
 
             System.out.println("Inorder traversal:");
             binaryTree.inorderTraversal();
